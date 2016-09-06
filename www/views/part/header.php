eb2b77e6faa0bf771a594733fee74c96d377d0a1
조용준 <junishop@gmail.com>
2016-09-06 21:30:03 +0900

    asd

--- www/views/part/header.php
+++ www/views/part/header.php
@@ -1,9 +1,132 @@
 <!-- Header.html -->
+<div id="skip_nav">
+	<a href="container">본문바로가기</a>
+</div>
 <div id="wrap">
 	<div id="header">
-		
-		
+		<div class="container">
+			<h1 class="site-logo"><a href="/"><span class="sr-only">한국벤처캐피탈협회 부설 R&amp;D사업화 투자협의회</span></a></h1>
+			<div class="lnb">
+				<ul>
+					<li>
+						<a href="#none">R&amp;D지원사업</a>                                    
+						<ul>
+							<li>
+								<a href="#none">사업안내</a>
+								<ul>
+									<li><a href="#none">투자연계관제안내</a></li>
+									<li><a href="#none">진행절차</a></li>
+									<li><a href="#none">제출서류 및 관련규정</a></li>
+								</ul>
+							</li>                        
+							<li>
+								<a href="#none">회원가입</a>
+								<ul>
+									<li><a href="#none">회원가입안내</a></li>
+									<li><a href="#none">투자기관 회원가입</a></li>
+								</ul>
+							</li>
+							<li>
+								<a href="#none">선정기업</a>
+								<ul>
+									<li><a href="#none">기업정보</a></li>
+									<li><a href="#none">반기 주요경영상황보고</a></li>
+									<li><a href="#none">제출서류 연차보고서 관련규정</a></li>
+									<li><a href="#none">수시보고서</a></li>
+									<li><a href="#none">비밀번호변경</a></li>
+								</ul>
+							</li>
+							<li>
+								<a href="#none">투자기관</a>
+							</li>
+							<li>
+								<a href="#none">신청기업</a>
+							</li>
+							<li>
+								<a href="#none">선정신청</a>
+							</li>
+							<li>
+								<a href="#none">투자기관</a>
+							</li>
+						</ul>
+					</li>
+					<li>
+						<a href="#none">투자지원사업</a>
+						<ul>
+							<li><a href="#none">벤처투자로드쇼</a></li>
+							<li><a href="#none">벤처투자사랑방</a></li>
+							<li><a href="#none">중소기업청 멘토링</a></li>
+							<li><a href="#none">사업부 멘토링</a></li>
+						</ul>
+					</li>
+					<li>
+						<a href="#none">Online IR</a>
+						<ul>
+							<li><a href="#none">Online IR</a></li>
+							<li><a href="#none">IR등록(수정)</a></li>
+							<li><a href="#none">실시간 IR</a></li>
+						</ul>
+					</li>
+					<li>
+						<a href="#none">정보마당</a>
+						<ul>
+							<li><a href="#none">공지사항</a></li>
+							<li><a href="#none">자료실</a></li>
+							<li><a href="#none">E-mail상담</a></li>
+							<li><a href="#none">FAQ</a></li>
+							<li><a href="#none">협회일정</a></li>
+						</ul>
+					</li>
+					<li>
+						<a href="#none">협의회소개</a>
+						<ul>
+							<li><a href="#none">협의회 안내</a></li>
+							<li><a href="#none">찾아오시는 길</a></li>
+						</ul>
+					</li>
+				</ul>
+			</div>
+			<!-- /.lnb -->
+			<ul class="gnb">
+				<li><a href="#none" class="btn-login">로그인</a></li>
+				<li><a href="#none">회원가입 </a></li>
+				<li><a href="#none">이용안내</a></li>
+				<li><a href="#none">사이트맵</a></li>
+				<li><a href="#none">FAQ</a></li>
+			</ul>
+			<!-- /.gnb -->
+			<a href="#none" class="btn-pds">자료 다운로드</a>
+		</div>
 	</div>
 	<div id="container">
 		<div class="container">
 <!-- Header.html -->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
